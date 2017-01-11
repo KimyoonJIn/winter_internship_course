b53576b3227132fa402263c5d22ff23675bd1a27
Tanima95 <tans_95@live.com>
2017-01-11 18:10:34 +0530

    Updated Based_on_issued_date_downloader.py

--- Based_on_issued_date_downloader.py
+++ Based_on_issued_date_downloader.py
@@ -16,20 +16,25 @@ def crawling_data_from_google():
     soup_grant = BeautifulSoup(google_grant_page.read(), 'html.parser')
     soup_application = BeautifulSoup(google_application_page.read(), 'html.parser')
     logging.info('Start to crawl google grant till 2015')
-    crawling_google_on_issued_date(soup_grant)
+    crawling_webpage_on_issued_date(soup_grant)
     logging.info('Start to crawl google application till 2015')
-    crawling_google_on_issued_date(soup_application)
+    crawling_webpage_on_issued_date(soup_application)
 
-
-def crawling_google_on_issued_date(soup_page):
-    for list in soup_page.findAll('h3'):
+def crawling_webpage_on_issued_date(soup_page):
+    for list in soup_page.findAll('h3') or list in soup_page.findAll('div', {'class', 'bulkyear'}):
         if list['id'] <= '2015':
             logging.info('start to crawl google patent files till 2015')
             for patent_link in soup_page.findAll('a'):
                 if patent_link['href'].endswith('.zip'):
                     list_google_patent.append(patent_link['href'])
             print(list_google_patent)
-
+        else:
+            logging.info('start to crawl reedtech patent file from 2016')
+            for patent_link in soup_page.findAll('a'):
+                if patent_link['href'].endswith('.zip'):
+                    list_reedtech_patent.append("http://patents.reedtech.com/" + patent_link['href'])
+            print(list_reedtech_patent)
+            
 
 def crawling_data_from_reedtech():
     reedtech_grant_fulltext_url = "http://patents.reedtech.com/pgrbft.php"
@@ -39,20 +44,12 @@ def crawling_data_from_reedtech():
     soup_grant = BeautifulSoup(grant_page.read(), 'html.parser')
     soup_application = BeautifulSoup(application_page.read(), 'html.parser')
     logging.info('start to crawl Reedtech grant from 2016')
-    crawling_reedtech_on_issued_date(soup_grant)
+    crawling_webpage_on_issued_date(soup_grant)
 
     logging.info('start to crawl Reedtech application from 2016')
-    crawling_reedtech_on_issued_date(soup_application)
+    crawling_webpage_on_issued_date(soup_application)
 
 
-def crawling_reedtech_on_issued_date(soup_page):
-    for list in soup_page.findAll('div', {'class', 'bulkyear'}):
-        if list['id'] > '2015':
-            logging.info('start to crawl reedtech patent file from 2016')
-            for patent_link in soup_page.findAll('a'):
-                if patent_link['href'].endswith('.zip'):
-                    list_reedtech_patent.append("http://patents.reedtech.com/" + patent_link['href'])
-            print(list_reedtech_patent)
 
 
 def get_filename_from_patent_url(patent_url):
